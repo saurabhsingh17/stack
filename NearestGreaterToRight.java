6374c03d2c14cc12821a2efa4ad8c34575bb77a9
saurabhsingh <saurabh623a@gmail.com>
2024-02-11 23:40:40 +0530

    Latest code

--- NearestGreaterToRight.java
+++ NearestGreaterToRight.java
@@ -4,32 +4,50 @@ import java.util.Stack;
 public class NearestGreaterToRight {
     public static void main(String[] args) {
         int[] a = {1,2,3,4};
-        System.out.println(Arrays.toString(nextGreaterElement(a)));
+        System.out.println(Arrays.toString(StackApproach.nextGreaterElement(a)));
+        System.out.println(Arrays.toString(StackApproachOptimised.nextGreaterElement(a)));
     }
-
-    public static int[] nextGreaterElement(int[] nums) {
-        int n = nums.length;
-        int[] result = new int[n];
-        Stack<Integer> stack = new Stack<>();
-        for (int i = n - 1; i >= 0; i--) {
-            if (stack.isEmpty()) {
-                result[i] = -1;
-            } else {
-                if (stack.peek() > nums[i]) {
-                    result[i] = stack.peek();
+    static class StackApproach {
+        public static int[] nextGreaterElement(int[] nums) {
+            int n = nums.length;
+            int[] result = new int[n];
+            Stack<Integer> stack = new Stack<>();
+            for (int i = n - 1; i >= 0; i--) {
+                if (stack.isEmpty()) {
+                    result[i] = -1;
                 } else {
-                    while (!stack.isEmpty() && stack.peek() < nums[i]) {
-                        stack.pop();
-                    }
-                    if (stack.isEmpty()) {
-                        result[i] = -1;
+                    if (stack.peek() > nums[i]) {
+                        result[i] = stack.peek();
                     } else {
-                        result[i] = nums[i];
+                        while (!stack.isEmpty() && stack.peek() < nums[i]) {
+                            stack.pop();
+                        }
+                        if (stack.isEmpty()) {
+                            result[i] = -1;
+                        } else {
+                            result[i] = nums[i];
+                        }
                     }
                 }
+                stack.push(nums[i]);
             }
-            stack.push(nums[i]);
+            return result;
         }
-        return result;
     }
+    static class StackApproachOptimised {
+        static int[] nextGreaterElement(int[] nums) {
+            int n = nums.length;
+            Stack<Integer> stack = new Stack<>();
+
+            for (int i = n-1; i >= 0; i--) {
+                int num = nums[i];
+                while (!stack.empty() && stack.peek() <= nums[i]) stack.pop();
+                nums[i] = stack.isEmpty() ? -1 : stack.peek();
+                stack.push(num);
+            }
+            return nums;
+        }
+    }
+
+
 }
