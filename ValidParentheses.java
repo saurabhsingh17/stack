27ccbe7e8d4c1d8560c290cd60d85a9e43baa53e
saurabhsingh <saurabh623a@gmail.com>
2024-02-15 23:42:05 +0530

    Latest code

---
+++ ValidParentheses.java
@@ -0,0 +1,30 @@
+import java.util.HashMap;
+import java.util.Stack;
+
+public class ValidParentheses {
+    public boolean isValid(String s) {
+        char[] ss = s.toCharArray();
+        HashMap<Character, Character> hashMap = new HashMap<>();
+        hashMap.put('}', '{');
+        hashMap.put(')', '(');
+        hashMap.put(']', '[');
+        Stack<Character> stack = new Stack<>();
+        for(char i: ss){
+            if(hashMap.containsKey(i)){
+                if(!stack.isEmpty() && stack.peek() == hashMap.get(i)){
+                    stack.pop();
+                }
+                else {
+                    return false;
+                }
+            }
+            else {
+                stack.push(i);
+            }
+        }
+        if(stack.isEmpty())
+            return true;
+        else
+            return false;
+    }
+}
